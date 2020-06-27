c32c7488c0340a1a59493455bf88d28e3a9137cb
Fátima Rojas Carhuas <64380871+Fatima2427@users.noreply.github.com>
2020-06-26 23:00:29 -0500

    listo
    actualizando

--- telusko/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from travello.views import index
-
-urlpatterns = [
-    path('', include ('travello.urls')),
-    path('admin/', admin.site.urls),
-]
