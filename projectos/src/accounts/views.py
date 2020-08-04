90e4a5e796af0e95e17a11172847b3a941134af6
Fátima Rojas Carhuas <64380871+Fatima2427@users.noreply.github.com>
2020-08-03 23:34:33 -0500

    Update views.py

--- projectos/src/accounts/views.py
+++ projectos/src/accounts/views.py
@@ -1,8 +1,18 @@
+from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-
+from .forms import FormPaciente
+from accounts.models import Paciente
+from django.views.generic import View
+from historias.utileria import render_pdf
+from django.http import HttpResponse
+from historias.utileria import render_pdf
 # Create your views here.
+# Create your views here.
+def index (request):
+
+	return render(request , "index.html")
 def login(request):
 	if request.method=='POST':
 		username=request.POST['username']
@@ -11,14 +21,19 @@ def login(request):
 		user= auth.authenticate(username=username,password=password)
 		if user is not None:
 			auth.login(request, user)
-			return redirect("/")
+			print('todo correcto')
+			return redirect('base')
+			
 		else:
 			messages.info(request,'invalid credentials')
 			return redirect('login')
+			print('fallo datos')
 	else:
 		return render(request, 'login.html')
+		print('fallo')
 
 def register(request):
+	paciente_form=FormPaciente()
 	if request.method =='POST':
 		first_name=request.POST['first_name']
 		last_name=request.POST['last_name']
@@ -26,27 +41,51 @@ def register(request):
 		password1=request.POST['password1']
 		password2=request.POST['password2']
 		email=request.POST['email']
+		paciente_form=FormPaciente(request.POST)
 
 		if password1==password2:
 			if User.objects.filter(username=username).exists():
 				messages.info(request, 'Username taken')
+				print('falso')
 				return redirect('register')
 			elif User.objects.filter(email=email).exists():
 				messages.info(request, 'email taken')
+				print('repetido')
 				return redirect('register')
 			else:
-				user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-				user.save();
-				print('user created')
-				return redirect('login')
+				if paciente_form.is_valid():
+					user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+					profile=paciente_form.save(commit=False)
+					profile.user=user
+					profile.save()
+					user.save()
+					print('user created')
+					return redirect('login')
 		
 		else:
 			print('password not maching..')
 			return redirect('register')
 		return redirect('/')
 	else:
-		return render(request, 'register.html')
+		context={'paciente_form':paciente_form}
+		return render(request, 'register.html', context)
 
 def logout(request):
 	auth.logout(request)
-	return redirect('/')
+	return render(request, '/')
+def base(request):
+	return render(request, 'accounts/base.html')
+
+def perfiles(request):
+    obj =Paciente.objects.get(user_id=6)
+    context={ 'objeto': obj, }
+    return render(request,'' accounts/perfil.html', context)
+
+class imprimir(View):
+	def get(self , request, *args, **kwargs):
+		obj=Paciente.objects.get(user_id=6)
+		context={ 'objeto': obj, }
+		pdf=render_pdf('accounts/mi_pdf.html', context)
+		return HttpResponse(
+			pdf, content_type='application/pdf'
+		)
